50e7eb04b49840624c60f599d2849a619c7931f4
mrgrease <mehmetemreyorulmaz@gmail.com>
2022-03-29 17:40:07 +0300

    Fix cors error

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,7 @@ app.route("/Images/:page").get(
         fetchImagesFromDb(page,function(result)
         {
             console.log(result);
+            res.set('Access-Control-Allow-Origin', '*');
             res.send(result);
         })
     }
